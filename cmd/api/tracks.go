37dad75c0d019639c7f476e77f88c4e9041ebd59
Ksolj <Stebakov-a.a@yandex.ru>
2024-11-15 04:57:09 +0300

    Now tabs can be put on the server as files. createTrackHandler changed

--- cmd/api/tracks.go
+++ cmd/api/tracks.go
@@ -3,20 +3,37 @@ package main
 import (
 	"errors"
 	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/ksolj/ongaku-api/internal/data"
 	"github.com/ksolj/ongaku-api/internal/data/validator"
 )
 
+const uploadDir = "./uploads/tabs"
+const baseFileURL = "http://localhost:4000/tabs"
+
 func (app *application) createTrackHandler(w http.ResponseWriter, r *http.Request) {
+	contentType := r.Header.Get("Content-Type")
+	if !strings.HasPrefix(contentType, "multipart/form-data") {
+		app.badRequestResponse(w, r, fmt.Errorf("Content-Type must be multipart/form-data"))
+		return
+	}
+
 	var input struct {
 		Name     string   `json:"name"`
 		Duration int32    `json:"duration"`
 		Artists  []string `json:"artists"`
 		Album    string   `json:"album"`
-		// Tabs []Tab `json:"tabs"` // TODO: will be implemented later
 	}
+
+	// Get json data from the request and convert it to the ReaderCloser to pass it to our readJSON helper.
+	jsonPart := r.FormValue("json_data")
+	jsonPartReader := strings.NewReader(jsonPart)
+	r.Body = io.NopCloser(jsonPartReader)
 
 	err := app.readJSON(w, r, &input)
 	if err != nil {
@@ -24,12 +41,34 @@ func (app *application) createTrackHandler(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
+	file, header, err := r.FormFile("file")
+	if err != nil {
+		app.badRequestResponse(w, r, fmt.Errorf("File must be provided"))
+	}
+	defer file.Close()
+
+	// This part is temporary 'cause in the future cloud object storage will be used (S3)
+	filePath := filepath.Join(uploadDir, header.Filename) // TODO: get rid of this later. Use S3
+	dst, err := os.Create(filePath)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+	defer dst.Close()
+
+	if _, err := io.Copy(dst, file); err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	fileURL := fmt.Sprintf("%s/%s", baseFileURL, header.Filename)
+
 	track := &data.Track{
 		Name:     input.Name,
 		Duration: input.Duration,
 		Artists:  input.Artists,
 		Album:    input.Album,
-		Tabs:     []data.Tab{"null"}, // TODO: implement!
+		Tabs:     fileURL,
 	}
 
 	v := validator.New()
