442a42f83f4c68ab93900d41e125ace8c2d4c6f6
Ksolj <Stebakov-a.a@yandex.ru>
2024-11-24 11:39:59 +0300

    User authentication/activation check middleware

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -15,10 +15,10 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 
 	router.HandlerFunc(http.MethodGet, "/v1/tracks", app.listTracksHandler)
-	router.HandlerFunc(http.MethodPost, "/v1/tracks", app.createTrackHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/tracks", app.requireActivatedUser(app.createTrackHandler))
 	router.HandlerFunc(http.MethodGet, "/v1/tracks/:id", app.showTrackHandler)
-	router.HandlerFunc(http.MethodPatch, "/v1/tracks/:id", app.updateTrackHandler)
-	router.HandlerFunc(http.MethodDelete, "/v1/tracks/:id", app.deleteTrackHandler)
+	router.HandlerFunc(http.MethodPatch, "/v1/tracks/:id", app.requireActivatedUser(app.updateTrackHandler))
+	router.HandlerFunc(http.MethodDelete, "/v1/tracks/:id", app.requireActivatedUser(app.deleteTrackHandler))
 
 	router.HandlerFunc(http.MethodGet, "/v1/tabs/:filename", app.getTabsHandler)
 
